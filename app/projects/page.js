5c8a8fff68afe69b4ac7057ea1cdb0d51c6d0a62
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2026-02-10 10:08:43 -0500

    updated projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -23,17 +23,22 @@ export default function Projects() {
                             {"tool":"Firebase DB", "color":"bg-red-300"},
                         ]}/>
                         <ProjectDiv link = 'https://github.com/ohi-711/Rest' title = 'Rest' content = 'An AI music therepy site. It implements machine vision to detect your mood and uses music to improve it.' img = '/Rest Image.jpg' tools = {[
+                            {"tools":"Python", "color":"bg-blue-500"},
                             {"tool":"JavaScript", "color":"bg-yellow-300"},
                             {"tool":"Flask", "color":"bg-sky-400"},
                             {"tool":"OpenAI", "color":"bg-gray-300"}
                         ]}/>
                     </div>
                     <div className="flex flex-row flex-wrap items-center justify-center">
-                        <ProjectDiv link = 'https://github.com/KennethWang1/Stock-AI' title = 'Stock AI' content = '' tools = {[
-                            
+                        <ProjectDiv link = 'https://github.com/KennethWang1/Stock-AI' title = 'Stock AI' content = 'A neural network-based stock trading algorithm. At the end of the trading day, it gets all relvent stock data as well as news data and trains based on that.' img = './stock-ai.png' tools = {[
+                            {"tool":"Python", "color":"bg-blue-500"},
+                            {"tool":"Flask", "color":"bg-sky-400"},
+                            {"tool":"TensorFlow", "color":"bg-orange-500"}
                         ]}/>
-                        <ProjectDiv link = 'https://google.com' title = '' content = ''/>
-                        <ProjectDiv link = 'https://google.com' title = '' content = '' tools = {[
+                        <ProjectDiv link = 'https://github.com/KennethWang1/Disease-Heatmap' title = 'Disease Heatmap' content = 'A anonymous crowd-sourced disease tracking app. It uses self-reported data to track what diseases people are getting in the area.' img = 'disease heatmap.jpg' tools={[
+                            {"tool":"React Native", "color":"bg-blue-500"},
+                            {"tool":"Node.js", "color":"bg-green-500"},
+                            {"tool":"MongoDB", "color":"bg-green-700"},
                         ]}/>
                     </div>
                 </div>
@@ -52,7 +57,7 @@ function ProjectDiv({content, link, img = "https://placehold.co/800x500", title 
                 <div className="bg-yellow-500 b-0 w-2.5 h-2.5 m-4 ml-0 rounded-full mt-2 mb-2" />
             </div>
             <div className='flex h-full bg-[rgb(10,15,41)] m-0 flex-col items-center p-2 pb-0 rounded-b-lg'>
-                <img src={img} alt="Project Image" className='rounded-lg top-0'/>
+                <img src={img} alt="Project Image" className='rounded-lg w-full h-48 object-cover overflow-hidden'/>
                 <div className='w-full h-40 mt-4 mb-4 rounded-lg overflow-hidden flex flex-col items-center px-2'>
                     <a href = {link} rel="noopener noreferrer" target="_blank" className='text-foreground relative w-fit underline decoration-foreground text-center pl-2 text-2xl overflow-hidden'>{title}</a>
                     <p className='text-white pl-2 pr-2'>
